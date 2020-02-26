0dddc0a283e03ad0f0d45bb6ee81e08620ff1f96
Matej Kupciha <kupciham@hotmail.com>
2020-02-26 12:01:41 +0100

    Add exception message to retry logs

--- kupcimat/util.py
+++ kupcimat/util.py
@@ -7,8 +7,9 @@ def with_retry(callback, callback_name, exception, count=3, sleep=1):
     for i in range(count - 1):
         try:
             return callback()
-        except exception:
-            logging.debug("action=retry callback=%s exception=%s count=%s", callback_name, exception.__name__, i)
+        except Exception as e:
+            logging.debug("action=retry callback=%s exception=%s attempt=%s message=%s",
+                          callback_name, exception.__name__, i, e)
             time.sleep(sleep)
     return callback()
 
